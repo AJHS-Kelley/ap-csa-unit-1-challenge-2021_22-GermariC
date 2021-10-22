02892dc9a4a476463c4c7cced828a318e2e002a3
Randy Kaneer <s11306958@students.duvalschools.org>
2021-10-22 09:05:59 -0400

    Peer Debugging
    The Name of the File needs to match up with the Class Name

--- Unit1Challenge.java
+++ Unit1Challenge.java
@@ -1,5 +1,6 @@
 // <Germari Cull>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <October 20, 2021>, <8:56>, v0.0
 //Peer Debugging by Randy K.
+//You need to change the name of the file to DamageDealt.Java to match the Class Name
 public class DamageDealt {
 
 }
