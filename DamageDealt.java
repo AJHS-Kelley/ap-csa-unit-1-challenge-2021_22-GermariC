1dc73ff8508451c12c45cb7b0225a85b9d52cabe
s11028834 <s11028834@students.duvalschools.org>
2021-10-26 09:39:47 -0400

    v0.4

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,5 +1,5 @@
 
-// <Germari Cull>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <October 26, 2021>, <9:07>, v0.3
+// <Germari Cull>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <October 26, 2021>, <9:39>, v0.4
 // Peer Debugging by Lorenzo Kenon
 import java.util.Scanner;
 
@@ -20,6 +20,10 @@ public class DamageDealt {
       System.out.println("The Name of your monster is:" + nameOfMonster );
 
       System.out.println("How many attacks will the player make?:  ");
+
+      String numOfAttacks = myScanner.int();
+
+      System.out.println("Your player made")
 
         System.out.println("How much damage will the attack do?: ");
         
