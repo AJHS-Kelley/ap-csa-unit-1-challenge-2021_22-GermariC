870241ac4c614b0d7e926cdd61d12b5f515ad6cd
s11028834 <s11028834@students.duvalschools.org>
2021-10-26 08:41:46 -0400

    v0.2

---
+++ DamageDealt.java
@@ -0,0 +1,19 @@
+
+// <Germari Cull>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <October 26, 2021>, <8:41>, v0.2
+// Peer Debugging by Lorenzo Kenon
+import java.util.Scanner;
+
+public class DamageDealt {
+    public static void main(string[] args){
+      Scanner myScanner = new Scanner(System.in)
+      
+      System.out.println("What is the name of your Weapon/attack?:  ");
+
+      String 
+       System.out.println("What is the name of your monster?: ");
+        System.out.println("How many attacks will the player make?:  ");
+        System.out.println("How much damage will the attack do?: ");
+        
+
+    }
+}
--- Unit1Challenge.java
+++
@@ -1,9 +0,0 @@
-// <Germari Cull>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <October 20, 2021>, <8:56>, v0.1
-// Peer Debugging by Lorenzo Kenon
-public class DamageDealt {
-    public static void main(string[] args){
-        System.out.println("What is the name of your weapon/attack?:  ")
-
-
-    }
-}
