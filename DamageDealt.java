f2012b7bea237f1e2fc3f905f36a46d1209e1b64
s11028834 <s11028834@students.duvalschools.org>
2021-10-26 09:08:12 -0400

    v0.3

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,17 +1,26 @@
 
-// <Germari Cull>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <October 26, 2021>, <8:41>, v0.2
+// <Germari Cull>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <October 26, 2021>, <9:07>, v0.3
 // Peer Debugging by Lorenzo Kenon
 import java.util.Scanner;
 
 public class DamageDealt {
     public static void main(string[] args){
-      Scanner myScanner = new Scanner(System.in)
+      Scanner myScanner = new Scanner(System.in);
       
-      System.out.println("What is the name of your Weapon/attack?:  ");
+      System.out.println("What is the name of your weaponOrAttack?:  ");
 
-      String 
-       System.out.println("What is the name of your monster?: ");
-        System.out.println("How many attacks will the player make?:  ");
+      String weaponOrAttack = myScanner.nextline();
+
+      System.out.println("Your Weapon/attack is called:" + weaponOrAttack)
+
+      System.out.println("What is the name of your monster?: ");
+      
+      String nameOfMonster = myScanner.nextline();
+
+      System.out.println("The Name of your monster is:" + nameOfMonster );
+
+      System.out.println("How many attacks will the player make?:  ");
+
         System.out.println("How much damage will the attack do?: ");
         
 
