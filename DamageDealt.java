147e4df3e4b37936d575c6106a10b51480ac0411
s11028834 <s11028834@students.duvalschools.org>
2021-10-28 08:52:50 -0400

    v0.5

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,31 +1,33 @@
 
-// <Germari Cull>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <October 26, 2021>, <9:39>, v0.4
+// <Germari Cull>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <October 28, 2021>, <8:52>, v0.5
 // Peer Debugging by Lorenzo Kenon
 import java.util.Scanner;
 
 public class DamageDealt {
-    public static void main(string[] args){
+  public static void main(string[] args){
       Scanner myScanner = new Scanner(System.in);
       
       System.out.println("What is the name of your weaponOrAttack?:  ");
 
-      String weaponOrAttack = myScanner.nextline();
+      String weaponOrAttack = myScanner.nextLine();
 
       System.out.println("Your Weapon/attack is called:" + weaponOrAttack)
 
       System.out.println("What is the name of your monster?: ");
       
-      String nameOfMonster = myScanner.nextline();
+      String nameOfMonster = myScanner.nextLine();
 
       System.out.println("The Name of your monster is:" + nameOfMonster );
 
       System.out.println("How many attacks will the player make?:  ");
 
-      String numOfAttacks = myScanner.int();
+      String numOfAttacks = myScanner.nextInt();
 
-      System.out.println("Your player made")
+      System.out.println("Your player made: ");
 
-        System.out.println("How much damage will the attack do?: ");
+      String numOfDamage = myScanner.nextInt();
+
+      System.out.println("How much damage will the attack do?: ");
         
 
     }
